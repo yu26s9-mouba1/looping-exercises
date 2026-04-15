929533c4970c0adaeae5bfe6bc7af249b63080d8
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-15 15:03:32 -0400

    I used a while loop to print a sentence five times

---
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        // Using a while loop, I will print the sentence "I love Java" five times.
+
+        int count = 0;  //Setting the counter at 0
+        while (count < 5){  //Making sure it stops at 5
+            System.out.println("I Love Java");  //Printing the sentence now
+            count++;    // Adding 1 at ech count untill it satid=fies five times
+
+        }
+
+    }
+}
