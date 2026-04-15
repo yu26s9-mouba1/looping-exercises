0579ff426a8a7d47997aedafa993aed8c3dd59b5
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-15 15:21:06 -0400

    Used a do while loop to print a statement five times

---
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        // Using a do while loop to print a sentence 5 times
+
+//        int count = 1;
+//        String love = "I Love Java";
+//        do {
+//            System.out.println(love);
+//            count++;
+//
+//        }while (count <= 5);
+//
+        // Using an advanced version
+      int count = 0;
+      int max = 5;
+
+      do{
+          System.out.println("I Love Java");
+          count ++;
+
+      }while(count< max);
+
+
+
+
+
+    }
+}
